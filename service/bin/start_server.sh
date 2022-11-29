9aa2226c348eca0685745ec0c2f1906cd268d6f6
mostafab <mostafa0104@gmail.com>
2022-11-29 17:03:11 -0500

    Move service classes

--- service/bin/start_server.sh
+++ service/bin/start_server.sh
@@ -4,4 +4,5 @@ set -e
 
 cd $(dirname "$0")
 cd ..
+mvn compile
 mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 neg5.service.Main"
