d0b5d1ae943ec65a9f5f340feef85bd29acf558e
mostafab <mostafa0104@gmail.com>
2022-11-29 16:34:20 -0500

    Add bash script to start service

---
+++ bin/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")
+cd ../service
+mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 org.neg5.Main"
