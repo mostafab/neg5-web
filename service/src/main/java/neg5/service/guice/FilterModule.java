7461e3c8384f606e16ec498aeb1d55b37ba03deb
mostafab <mostafa0104@gmail.com>
2022-11-30 10:47:34 -0500

    Add logging to exception handler

--- service/src/main/java/neg5/service/guice/FilterModule.java
+++ service/src/main/java/neg5/service/guice/FilterModule.java
@@ -3,6 +3,7 @@ package neg5.service.guice;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import neg5.service.filters.CurrentUserContextFilter;
+import neg5.service.filters.GeneralExceptionHandlerFilter;
 import neg5.service.filters.NoResultHandlerFilter;
 import neg5.service.filters.ObjectValidationExceptionHandlerFilter;
 import neg5.service.filters.RequestFilter;
@@ -19,5 +20,6 @@ public class FilterModule extends AbstractModule {
         multibinder.addBinding().to(CurrentUserContextFilter.class);
         multibinder.addBinding().to(TournamentAccessExceptionFilter.class);
         multibinder.addBinding().to(ObjectValidationExceptionHandlerFilter.class);
+        multibinder.addBinding().to(GeneralExceptionHandlerFilter.class);
     }
 }
