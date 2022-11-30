47814d327f46dc8e6740f52b17f0d5473160f2bb
mostafab <mostafa0104@gmail.com>
2022-11-29 19:20:08 -0500

    Add script which creates new tag on a successful deploy

--- service/bin/deploy_heroku.sh
+++ service/bin/deploy_heroku.sh
@@ -7,6 +7,8 @@ cd ../..
 mvn clean
 mvn install
 # Get confirmation of deploy
-read -p "Proceed with deploy to Heroku? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+#read -p "Proceed with deploy to Heroku? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 cd service/
-mvn heroku:deploy
+#mvn heroku:deploy
+cd bin/
+./create_new_tag.sh -v major
