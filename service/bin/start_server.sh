bd0872e0a67d3187a8b4a30859dc11b90e6c6a65
mostafab <mostafa0104@gmail.com>
2022-12-02 15:49:00 -0500

    Update start_server.sh

--- service/bin/start_server.sh
+++ service/bin/start_server.sh
@@ -6,6 +6,7 @@ debug_port=1044
 entrypoint="neg5.service.Main"
 
 cd $(dirname "$0")
-cd ..
+cd ../../
 mvn compile
+cd service/
 mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$debug_port $entrypoint"
