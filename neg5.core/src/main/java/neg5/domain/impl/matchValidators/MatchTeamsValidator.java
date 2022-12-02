4929426bb87ba522f93377876620e85e8535ca5b
mostafab <mostafa0104@gmail.com>
2022-12-01 22:10:45 -0500

    validation

--- neg5.core/src/main/java/neg5/domain/impl/matchValidators/MatchTeamsValidator.java
+++ neg5.core/src/main/java/neg5/domain/impl/matchValidators/MatchTeamsValidator.java
@@ -4,12 +4,15 @@ import static neg5.validation.FieldValidation.requireCondition;
 import static neg5.validation.FieldValidation.requireNotNull;
 
 import com.google.inject.Singleton;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import neg5.domain.api.FieldValidationErrors;
 import neg5.domain.api.MatchTeamDTO;
 import neg5.domain.api.TournamentMatchDTO;
+import neg5.domain.api.TournamentPlayerDTO;
+import neg5.domain.api.TournamentTeamDTO;
 
 @Singleton
 public class MatchTeamsValidator implements TournamentMatchValidator {
@@ -41,6 +44,41 @@ public class MatchTeamsValidator implements TournamentMatchValidator {
         requireCondition(
                 errors, teamIds.size() == 2, "teams", "A team cannot play against itself.");
 
+        subject.getTeams()
+                .forEach(
+                        team ->
+                                ensureTeamHasAllCorrectPlayers(
+                                        errors,
+                                        team,
+                                        validationContext.getTeamNamesById(),
+                                        validationContext.getPlayerNamesById()));
         return errors;
+    }
+
+    private void ensureTeamHasAllCorrectPlayers(
+            FieldValidationErrors errors,
+            MatchTeamDTO matchTeam,
+            Map<String, TournamentTeamDTO> teamsById,
+            Map<String, TournamentPlayerDTO> playersById) {
+        if (matchTeam.getPlayers() != null
+                && teamsById != null
+                && playersById != null
+                && teamsById.containsKey(matchTeam.getTeamId())) {
+            matchTeam.getPlayers().stream()
+                    .filter(player -> playersById.containsKey(player.getPlayerId()))
+                    .forEach(
+                            player -> {
+                                TournamentPlayerDTO tournamentPlayer =
+                                        playersById.get(player.getPlayerId());
+                                requireCondition(
+                                        errors,
+                                        matchTeam.getTeamId().equals(tournamentPlayer.getTeamId()),
+                                        "matchTeam.players",
+                                        String.format(
+                                                "%s appears as a player in this match, but does not belong to %s.",
+                                                tournamentPlayer.getName(),
+                                                teamsById.get(matchTeam.getTeamId()).getName()));
+                            });
+        }
     }
 }
