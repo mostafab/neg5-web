abbe404ec6d0f8e978ba8ef6f9021986c84f96da
mostafab <mostafa0104@gmail.com>
2022-12-05 11:36:16 -0500

    Add API tests, move GSON classes to new module

---
+++ automation/src/test/java/neg5/automation/AccountApiTest.java
@@ -0,0 +1,61 @@
+package neg5.automation;
+
+import static io.restassured.RestAssured.given;
+import static neg5.automation.ApiParsingUtilities.doRequestAndParse;
+import static neg5.automation.ApiParsingUtilities.toJsonString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+import neg5.domain.api.AccountCreationDTO;
+import neg5.domain.api.AccountDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AccountApiTest extends BaseApiTest {
+
+    private AccountCreationDTO account;
+    private AccountDTO result;
+
+    @BeforeEach
+    public void createAccount() {
+        account = new AccountCreationDTO();
+        String randomString = NanoIdUtils.randomNanoId();
+        account.setEmail(String.format("first.last.%s@domain.com", randomString));
+        account.setName("First Last");
+        account.setPassword(randomString);
+        account.setUsername(String.format("first-last-%s", randomString));
+
+        result =
+                doRequestAndParse(
+                        AccountDTO.class,
+                        () ->
+                                given().body(toJsonString(account))
+                                        .when()
+                                        .post("/neg5-api/accounts"));
+    }
+
+    @Test
+    public void testCreateAccount() {
+        assertEquals(account.getEmail(), result.getEmail());
+        assertEquals(account.getUsername(), result.getId());
+        assertEquals(account.getName(), result.getName());
+    }
+
+    @Test
+    public void testLoginValidCredentials() {
+        String body =
+                String.format(
+                        "{\"username\":\"%s\", \"password\":\"%s\"}",
+                        account.getUsername(), account.getPassword());
+        given().body(body).when().post("/neg5-api/login").then().statusCode(200).cookie("nfToken");
+    }
+
+    @Test
+    public void testLoginInValidCredentials() {
+        String body =
+                String.format(
+                        "{\"username\":\"%s\", \"password\":\"%s\"}",
+                        account.getUsername(), account.getPassword() + "garbage");
+        given().body(body).when().post("/neg5-api/login").then().statusCode(403);
+    }
+}
---
+++ automation/src/test/java/neg5/automation/User.java
@@ -0,0 +1,20 @@
+package neg5.automation;
+
+public class User {
+
+    private final String nfToken;
+    private final String username;
+
+    User(String nfToken, String username) {
+        this.nfToken = nfToken;
+        this.username = username;
+    }
+
+    public String getNfToken() {
+        return nfToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
