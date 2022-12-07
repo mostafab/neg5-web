7dde5f0329b3f6db67faa27f596ac32a244f5d85
mostafab <mostafa0104@gmail.com>
2022-12-07 11:46:18 -0500

    Spotless

--- service/src/main/java/neg5/service/guice/Neg5WebModule.java
+++ service/src/main/java/neg5/service/guice/Neg5WebModule.java
@@ -3,7 +3,7 @@ package neg5.service.guice;
 import com.google.inject.AbstractModule;
 import neg5.accessManager.guice.TournamentAccessGuiceModule;
 import neg5.configuration.guice.ConfigurationModule;
-import neg5.dataAccess.guice.DataAccessModule;
+import neg5.dataAccess.guice.PersistenceModule;
 import neg5.db.migrations.guice.DatabaseMigrationGuiceModule;
 import neg5.domain.guice.ApiLayerGuiceModule;
 import neg5.exports.qbj.guice.QbjGuiceModule;
@@ -17,7 +17,7 @@ public class Neg5WebModule extends AbstractModule {
     protected void configure() {
         install(new ConfigurationModule());
         install(new MonitoringGuiceModule());
-        install(new DataAccessModule());
+        install(new PersistenceModule());
         install(new DatabaseMigrationGuiceModule());
         install(new ApiLayerGuiceModule());
         install(new TournamentStatsModule());
