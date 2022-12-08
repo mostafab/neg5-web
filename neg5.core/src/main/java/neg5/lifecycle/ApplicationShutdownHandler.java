f5d2c901b628bf5f34267f827bd7c01b97e4ca26
mostafab <mostafa0104@gmail.com>
2022-12-07 20:48:55 -0500

    Add ApplicationShutdownHandler to close data source

--- neg5.core/src/main/java/neg5/lifecycle/ApplicationShutdownHandler.java
+++ neg5.core/src/main/java/neg5/lifecycle/ApplicationShutdownHandler.java
@@ -1,7 +1,10 @@
 package neg5.lifecycle;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,21 +13,28 @@ public class ApplicationShutdownHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationShutdownHandler.class);
 
+    private final List<Closeable> closeables;
+
+    @Inject
+    public ApplicationShutdownHandler() {
+        this.closeables = new ArrayList<>();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(this::closeAll));
+    }
+
     public void registerCloseable(Closeable closeable) {
         LOGGER.info("Registering closeable object {}", closeable.getClass());
-        Runtime.getRuntime()
-                .addShutdownHook(
-                        new Thread(
-                                () -> {
-                                    try {
-                                        LOGGER.warn("Attempting to close {}", closeable.getClass());
-                                        closeable.close();
-                                    } catch (Exception e) {
-                                        LOGGER.error(
-                                                "Encountered exception closing object "
-                                                        + closeable.getClass(),
-                                                e);
-                                    }
-                                }));
+        this.closeables.add(closeable);
+    }
+
+    private void closeAll() {
+        for (Closeable closeable : closeables) {
+            try {
+                LOGGER.warn("Attempting to close {}", closeable.getClass());
+                closeable.close();
+            } catch (Exception e) {
+                LOGGER.error("Encountered exception closing object " + closeable.getClass(), e);
+            }
+        }
     }
 }
