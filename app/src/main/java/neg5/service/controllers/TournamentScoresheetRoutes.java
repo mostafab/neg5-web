557b17ca38bafa619b1d34eb35ec135d5821d95f
mostafab <mostafa0104@gmail.com>
2023-01-08 18:16:52 -0800

    Endpoint to fetch scoresheet by id

--- app/src/main/java/neg5/service/controllers/TournamentScoresheetRoutes.java
+++ app/src/main/java/neg5/service/controllers/TournamentScoresheetRoutes.java
@@ -1,14 +1,17 @@
 package neg5.service.controllers;
 
 import com.google.inject.Inject;
+import neg5.accessManager.api.TournamentAccessManager;
 import neg5.domain.api.ScoresheetDTO;
 import neg5.domain.api.TournamentScoresheetApi;
+import neg5.domain.api.enums.TournamentAccessLevel;
 import neg5.service.util.RequestHelper;
 
 public class TournamentScoresheetRoutes extends AbstractJsonRoutes {
 
     @Inject private TournamentScoresheetApi scoresheetApi;
     @Inject private RequestHelper requestHelper;
+    @Inject private TournamentAccessManager accessManager;
 
     @Override
     protected String getBasePath() {
@@ -17,6 +20,13 @@ public class TournamentScoresheetRoutes extends AbstractJsonRoutes {
 
     @Override
     public void registerRoutes() {
+        get(
+                "/:id",
+                (request, response) -> {
+                    Long id = Long.parseLong(request.params("id"));
+                    ensureHasPermissions(id);
+                    return scoresheetApi.get(id);
+                });
         post(
                 "/convert",
                 (request, response) ->
@@ -27,5 +37,11 @@ public class TournamentScoresheetRoutes extends AbstractJsonRoutes {
                 (request, response) ->
                         scoresheetApi.submitScoresheet(
                                 requestHelper.readFromRequest(request, ScoresheetDTO.class)));
+    }
+
+    private void ensureHasPermissions(Long scoresheetId) {
+        ScoresheetDTO scoresheet = scoresheetApi.get(scoresheetId);
+        accessManager.requireAccessLevel(
+                scoresheet.getTournamentId(), TournamentAccessLevel.COLLABORATOR);
     }
 }
