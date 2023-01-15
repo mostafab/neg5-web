c4790a56a9bfd6ce9b4f34d50d6bd278a9fd751e
mostafab <mostafa0104@gmail.com>
2023-01-15 10:53:03 -0800

    Stub schedule validator

--- core/src/main/java/neg5/domain/impl/validators/TournamentScheduleValidator.java
+++ core/src/main/java/neg5/domain/impl/validators/TournamentScheduleValidator.java
@@ -5,8 +5,11 @@ import static neg5.validation.FieldValidation.requireNotNull;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import neg5.domain.api.FieldValidationErrors;
 import neg5.domain.api.TournamentScheduleDTO;
@@ -47,7 +50,6 @@ public class TournamentScheduleValidator {
         matches.forEach(
                 match -> {
                     requireNotNull(errors, match.getRound(), "round");
-                    requireNotNull(errors, match.getScheduleId(), "scheduleId");
                     requireCondition(
                             errors,
                             match.getRound() == null || match.getRound() > 0,
@@ -80,5 +82,43 @@ public class TournamentScheduleValidator {
                         .collect(
                                 Collectors.toMap(
                                         TournamentTeamDTO::getId, TournamentTeamDTO::getName));
+        Map<String, Set<Integer>> teamRounds = new HashMap<>();
+        for (TournamentScheduledMatchDTO match : matches) {
+            if (match.getRound() == null) {
+                continue;
+            }
+            String team1Id = match.getTeam1Id();
+            String team2Id = match.getTeam2Id();
+            if (team1Id != null && team2Id != null) {
+                requireCondition(
+                        errors,
+                        !team1Id.equals(team2Id),
+                        "teamId",
+                        String.format(
+                                "Round %d: %s cannot play itself.",
+                                match.getRound(), teamNamesById.get(team1Id)));
+            }
+            processTeamCheck(errors, teamNamesById, teamRounds, match.getRound(), team1Id);
+            processTeamCheck(errors, teamNamesById, teamRounds, match.getRound(), team2Id);
+        }
+    }
+
+    private void processTeamCheck(
+            FieldValidationErrors errors,
+            Map<String, String> teamNames,
+            Map<String, Set<Integer>> teamRounds,
+            Integer round,
+            String teamId) {
+        if (teamId == null || round == null) {
+            return;
+        }
+        teamRounds.computeIfAbsent(teamId, (id) -> new HashSet<>());
+        requireCondition(
+                errors,
+                !teamRounds.get(teamId).contains(round),
+                "round",
+                String.format(
+                        "%s has multiple matches in round %d.", teamNames.get(teamId), round));
+        teamRounds.get(teamId).add(round);
     }
 }
