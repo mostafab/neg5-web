099a6ff911e9c11d13c24d49407083182abd8252
mostafab <mostafa0104@gmail.com>
2023-01-19 23:43:57 -0500

    Client call for jwks

--- google/src/main/java/neg5/google/oauth/impl/GoogleOauthValidatorImpl.java
+++ google/src/main/java/neg5/google/oauth/impl/GoogleOauthValidatorImpl.java
@@ -9,6 +9,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -16,10 +17,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import neg5.google.oauth.api.DiscoveryDocument;
-import neg5.google.oauth.api.GoogleJwtTokenFields;
 import neg5.google.oauth.api.GoogleOauthCredentials;
 import neg5.google.oauth.api.GoogleOauthValidator;
 import retrofit2.Call;
@@ -29,6 +31,8 @@ public class GoogleOauthValidatorImpl implements GoogleOauthValidator {
     private final GoogleOpenIdClient openIdClient;
     private final Gson gson;
     private final Supplier<DiscoveryDocument> discoveryDocumentCache;
+    private static final Set<String> VALID_ISSUERS =
+            Sets.newHashSet("https://accounts.google.com", "accounts.google.com");
 
     @Inject
     public GoogleOauthValidatorImpl(GoogleOpenIdClient openIdClient, Gson gson) {
@@ -44,9 +48,10 @@ public class GoogleOauthValidatorImpl implements GoogleOauthValidator {
         if (!verifySignature(decodedJWT)) {
             return false;
         }
-        GoogleJwtTokenFields token =
-                gson.fromJson(decodedJWT.getPayload(), GoogleJwtTokenFields.class);
-        return true;
+        if (!VALID_ISSUERS.contains(decodedJWT.getIssuer())) {
+            return false;
+        }
+        return !Instant.now().isAfter(decodedJWT.getExpiresAtAsInstant());
     }
 
     private boolean verifySignature(DecodedJWT decodedJWT) {
